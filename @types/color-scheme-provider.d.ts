0f80b89cc5bc58765c28b242974bc81e1f66a18d
sagemodeninja <contact@garyantier.com>
2023-06-10 21:09:11 +0800

    ref: messaging
    + Change notification from direct callback invoke to message event API
    + Prettier

--- @types/color-scheme-provider.d.ts
+++ @types/color-scheme-provider.d.ts
@@ -1,10 +1,11 @@
 declare module '@sagemodeninja/color-scheme-provider' {
     type Callback = () => void;
-  
+
     class ColorSchemeProvider {
-      preferredColorScheme: string;
-      colorScheme: string;
-      subscribeNotification(callback: Callback): void;
+        preferredColorScheme: string;
+        colorScheme: string;
+        subscribeNotification(callback: Callback): void;
+        toggle(): void;
     }
 
     const colorSchemeProvider: ColorSchemeProvider;
